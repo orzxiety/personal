784d2557b21ebcbb8c64eb176ce95ececd44236f
Orzxiety <83078982+orzxiety@users.noreply.github.com>
2025-01-09 22:40:33 +0500

    Use $app/state instead of $app/stores

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import { page } from "$app/stores";
+    import { page } from "$app/state";
     import { onMount } from "svelte";
 
     onMount(() => {
-        if ($page.status === 404) {
+        if (page.status === 404) {
             goto('/')
         }
     })
 </script>
 
-{#if $page.status !== 404}
-    <h1 class="text-center">{$page.status}: {$page.error?.message}</h1>
+{#if page.status !== 404}
+    <h1 class="text-center">{page.status}: {page.error?.message}</h1>
 {/if}
